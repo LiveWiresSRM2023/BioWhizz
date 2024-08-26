725ca94f53f7b07ba008d1a9a4c472e258a3e7d6
vaseem15905 <138885639+vaseem15905@users.noreply.github.com>
2024-08-27 00:28:24 +0530

    fix:login ui updated

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -28,8 +28,8 @@ function Signin() {
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-green-100 to-yellow-100 p-4">
       <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full transition-transform transform hover:scale-105 hover:shadow-2xl">
         <div className="flex flex-col items-center mb-6">
-          <img src={biowhizzlogo} alt="Biowhizz Logo" className="w-40 h-40 mb-4" />
-          <p className="text-center text-green-800">
+          <img src={biowhizzlogo} alt="Biowhizz Logo" className="w-40 animate-fadeIn h-40 mb-4" />
+          <p className="text-center animate-fadeIn  text-green-800">
             Your smart, all-in-one solution for effortless biogas management and optimization.
           </p>
         </div>
